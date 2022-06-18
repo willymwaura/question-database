fcb93559911f5bdb7f9cb86be75f5551ac89b5f9
willymwaura <wilsonmwaura697@gmail.com>
2022-06-18 21:57:03 +0900

    admin

--- myapp/models.py
+++ myapp/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Feature (models.Model):
     question=models.CharField(max_length=1000)
     date=models.DateTimeField(auto_now_add=True,blank=True)
+    def __str__(self):
+        return '{}'.format(self.question)
 
 
 
